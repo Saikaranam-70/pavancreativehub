1dd0528d2000eced6598d0e3de2e26eb86ce2425
Saikaranam-70 <saikaranam46@gmail.com>
2024-08-23 22:48:39 +0530

    first commit

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,2 +1,2 @@
-export const API = 'http://localhost:4500'
-// export const API = 'https://pavancreativehub-backend1.onrender.com'
+// export const API = 'http://localhost:4500'
+ export const API = 'https://pavancreativehub-backend1.onrender.com'
